62b0fc5f5f765e97860e0a9e46c1391a278be9b1
fawwazaid <fawwazaid@gmail.com>
2025-11-01 15:50:04 +0700

    update path

--- proyek_analisis_data/dashboard.py
+++ proyek_analisis_data/dashboard.py
@@ -65,7 +65,7 @@ def create_rfm_df(df):
     return rfm_df
 
 # Load cleaned data
-all_df = pd.read_csv("all_data.csv")
+all_df = pd.read_csv("proyek_analisis_data/all_data.csv")
 
 datetime_columns = ["order_date", "delivery_date"]
 all_df.sort_values(by="order_date", inplace=True)
